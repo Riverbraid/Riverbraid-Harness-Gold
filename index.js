445cf6fae78b2d5150d788a13a266a8c89f2a4cb
Riverbraid <michael.tilk@gmail.com>
2026-03-30 21:32:13 +0000

    fix: displace legacy root 08e829 with de2062 - stationary floor

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
  */
 
 export const runInHarness = async (task, context) => {
-    const REQUIRED_ROOT = "08e829";
+    const REQUIRED_ROOT = "de2062";
     
     console.log(`[HARNESS] Initializing task: ${task.name}`);
 
